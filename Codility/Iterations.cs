77cddccaee9966d903e3e96ce47b409af26197db
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-11 18:32:26 +0100

    Add project files.

---
+++ Codility/Iterations.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Codility
+{
+    internal class Fib
+    {
+        public static void Fibbo(int until)
+        {
+            int a = 0; int b = 1; int c;
+
+            while (a <= until)
+            {
+                Debug.WriteLine(a);
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+
+        public static int BinaryGap(int N)
+        {
+            string binaryString = Convert.ToString(N, 2);
+            Debug.WriteLine(binaryString);
+
+            int indexFor1 = -1;
+            int largestBinaryGap = 0;
+            for (int i = 0; i < binaryString.Length; i++)
+            {
+                while (binaryString[i] != '1')
+                {
+                    i++;
+                    if (i == binaryString.Length) return largestBinaryGap;
+                }
+                
+                Debug.WriteLine("Reached a 1, at index " + i);
+
+                if (indexFor1 >= 0)
+                {
+                    int binaryGap = i - indexFor1 - 1;
+                    Debug.WriteLine("binaryGap: " + binaryGap);
+                    if (binaryGap > largestBinaryGap)
+                    {
+                        largestBinaryGap = binaryGap;
+                    }
+                    Debug.WriteLine("largestBinaryGap: " + largestBinaryGap);
+                }
+
+               indexFor1 = i;
+
+            }
+
+            return largestBinaryGap;
+        }
+    }
+
+}
---
+++ Codility/Program.cs
@@ -0,0 +1,85 @@
+﻿using System.Diagnostics;
+
+namespace Codility
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Redirect console output to Output window
+            var listener = new ConsoleTraceListener();
+            Debug.Listeners.Add(listener);
+
+            //PrintPyramid(4);
+            //PrintPyramidWithSpaces(4);
+            //PrintSimplePyramidWithSpaces(4);
+
+            //Fib.Fibbo(22);
+
+            Debug.WriteLine(Fib.BinaryGap(9));
+            Debug.WriteLine("");
+            Debug.WriteLine(Fib.BinaryGap(529));
+            Debug.WriteLine("");
+            Debug.WriteLine(Fib.BinaryGap(20));
+            Debug.WriteLine("");
+
+            // Keep the console window open
+            //Console.ReadKey();
+        }
+
+        private static void PrintSimplePyramidWithSpaces(int noOfLines)
+        {
+            for (int i = noOfLines; i > 0; i--)
+            {
+                for (int j = 0; j < noOfLines - i; j++)
+                {
+                    Debug.Write("_");
+                }
+
+                for (int k = 0; k < (i - 1) * 2 + 1; k++)
+                {
+                    Debug.Write("*");
+                }
+
+                Debug.WriteLine("");
+            }
+        }
+
+        public static void PrintPyramid(int noOfLines)
+        {
+            for (int i = noOfLines; i > 0; i--)
+            {
+                for (int j = 1; j <= (i * 2 - 1); j++)
+                {
+                    Debug.Write("*");
+                }
+                Debug.WriteLine("");
+            }
+            Debug.WriteLine("");
+        }
+
+        public static void PrintPyramidWithSpaces(int noOfLines)
+        {
+            int noOfLineIndex = noOfLines - 1;
+            int noOfColumns = noOfLineIndex * 2 + 1;
+            for (int row = noOfLineIndex; row >= 0; row--)
+            {
+                int noInThisRow = row * 2 + 1;
+                for (int column = 0; column < noOfColumns; column++)
+                {
+                    if (column >= noOfColumns - noInThisRow)
+                    {
+                        Debug.Write("*");
+                    } else
+                    {
+                        Debug.Write("_");
+                    }
+                }
+                
+                Debug.WriteLine("");
+            }
+            Debug.WriteLine("");
+        }
+    }
+}
