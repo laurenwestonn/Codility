4178c5b3853c8a1d053b2fc09758029837a66daa
LaurenJourney <161356914+LaurenJourney@users.noreply.github.com>
2024-06-21 11:17:57 +0100

    Triangle Sorting Problem - init

---
+++ Codility/Sorting.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace Codility
+{
+    internal class Sorting
+    {
+        public static int Triangle(int[] A) 
+        {
+            return 0;
+        }
+    }
+
+}
